d16d5f0f191a7d4b98ceb34d7ef05fab34d87c15
Peter Brightmore <P.C.Brightmore@gmail.com>
2023-01-19 14:45:12 +0000

    add utility rate check

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 function getName() {
-let myName = prompt ("What is your name?");
- return document.write(myName);
+    let myName = prompt ("What is your name?");
+    document.write(myName);
 }
-function getAge() {
-    if (age <= 18) {
+function getAge() { 
+    let age = prompt ("What is your age? (1-99)");
 
-        message = "We hope this website saves you moeny in future.";
-    
-    } else if (age <= 65) {
-        message = "We hope this website saves you money.";
+    let message;
+
+if (age <= 18) {
+        message = "we hope this website saves you money in future.";
+    } else if (age <= 64) {
+        message = "we hope this website saves you money.";
     } else if (age > 65 ) {
-        message = "We hope this website saves you money in your retirement.";
+        message = "we hope this website saves you money in your retirement.";
     } else {
-        message = "We hope this website can save you money at any age.";
+        message = "we hope this website can save you money at any age.";
     }
+    document.write(message);
 }   
+function getRate() {
+    let rate = prompt ("What is your unit rate (1.0-99.99)");
+    
+    let units = prompt ("How many units of energy have you used this month? (1.0-999999.99)");
+ 
+    let bill = ("Your estimated bill this month is £" + rate * units + "!");
+    document.write(bill);
+}
+
 // let time = prompt("What hour of the day is it? (0-23)");
 // let message;
 // console.log(time);
